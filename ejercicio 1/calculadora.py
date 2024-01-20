473587feb510078f9799ad1669a4da6d96c103a8
felipe <luisfelipegc79@gmail.com>
2024-01-20 15:28:38 -0500

    ejercicio1 completado

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,19 +1,31 @@
+# definir la funcion
 def calculadora():
-    num1 = float("Ingrese el primer número: ")
-    num2 = float("Ingrese el segundo número: ")
+    # guardar un valor tipo float
+    num1 = float(input("Ingrese el primer número: ")) # convertir el string en float agregando un input
+    # guardar un valor tipo float
+    num2 = float(input("Ingrese el segundo número: ")) # convertir el string en float agregando un input
+    # escoger una operacion matematica suma, resta, multiplicacion o division
     operacion = input("Ingrese la operación (+, -, *, /): ")
-
+    # condicion si el usuario escoge +
     if operacion == '+':
-        resultado = num + num2
+        #operacion si se cumple la condicion
+        resultado = num1 + num2 # falta colocar el 1
+    # condicion si el usuario escoge -
     elif operacion == '-':
+        #operacion si se cumple la condicion
         resultado = num1 - num2
+    # condicion si el usuario escoge *
     elif operacion == '*':
+        #operacion si se cumple la condicion
         resultado = num1 * num2
+    # condicion si el usuario escoge /
     elif operacion == '/':
-        resultado = num / num2
+        #operacion si se cumple la condicion
+        resultado = num1 / num2 # falta colocar el 1
+    # sino se cumple ninguna condicion anterior entonces escribir el mensaje
     else:
         resultado = "Operación no válida"
+    # imprimir el resultado de la opracion matematica
+    print(resultado) # quitar los caracteres Resultado:  
 
-    print("Resultado: "resultado)
-
-calculdora()
+calculadora() # a esta funcion le falta un caracter para quedar igual que el inicio del ejercicio
