6c88bb91cddf80c45855bb47c472f4a652cb41df
felipe <luisfelipegc79@gmail.com>
2024-01-20 16:39:22 -0500

    ejercicio completado

--- ejercicio 3/buscadorPalabras.py
+++ ejercicio 3/buscadorPalabras.py
@@ -1,6 +1,6 @@
 #llama a la funcion 
 def contar_palabra(texto, palabra): #falta colocar el caracter :
-    #devolver 
+    #convertir texto a minusculas con texto.lower, divide el texto con split, cuantas veces aparece la palabra buscada con .count
     return texto.lower().split().count(palabra.lower())
 #mostrar el mensaje
 texto = "Este es un ejemplo de texto. Este texto tiene palabras repetidas."
